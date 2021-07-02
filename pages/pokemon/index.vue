ba328980caa62ddeb25e0d17d9964904e7d6813f
Matheus Buenevides <matheusgomesbuenevides@gmail.com>
2021-07-02 00:38:28 -0300

    new page

---
+++ pages/pokemon/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex items-center flex-col h-screen justify-center gap-5">
+
+    <h1 class="pageTitle">Olá!</h1>
+
+    <ul v-for="item in data" :key="item.name">
+        <li>
+            {{item.name}}
+        </li>
+    </ul>
+    
+    
+  </div>
+
+</template>
+
+<script>
+export default {
+
+  async asyncData({ $axios }) {
+    const data = (await $axios.$get('/pokemon/')).results
+    return { data }
+
+  }
+
+
+}
+</script>
