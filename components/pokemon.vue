b6e2f8307fe274e492aa23e5e7963a9ae4070089
Matheus Buenevides <matheusgomesbuenevides@gmail.com>
2021-07-03 18:33:27 -0300

    New style component pokemon

--- components/pokemon.vue
+++ components/pokemon.vue
@@ -2,14 +2,20 @@
     <div v-if="show" class="flex flex-col h-screen items-center justify-center">
         <img v-bind:src="pokemon.img" alt="">
         
-        {{pokemon.name}}
-        <div>
+        <h1 class="pageTitle">{{pokemon.name}}</h1>
+        <div class="flex gap-5">
           <p>Altura: {{pokemon.height}}</p>
           <p>Peso: {{pokemon.weight}}</p>
         </div>
 
       </div>
 </template>
+
+<style>
+    p{
+        font-size: 18px;
+    }    
+</style>
 
 <script>
 export default {
@@ -20,6 +26,3 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
