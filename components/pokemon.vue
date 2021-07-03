6601fa88ae4b477f1566ab8058ded760b8f8175a
Matheus Buenevides <matheusgomesbuenevides@gmail.com>
2021-07-03 18:28:04 -0300

    New component and new style

---
+++ components/pokemon.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-if="show" class="flex flex-col h-screen items-center justify-center">
+        <img v-bind:src="pokemon.img" alt="">
+        
+        {{pokemon.name}}
+        <div>
+          <p>Altura: {{pokemon.height}}</p>
+          <p>Peso: {{pokemon.weight}}</p>
+        </div>
+
+      </div>
+</template>
+
+<script>
+export default {
+    props: {
+        pokemon: Object,
+        show: Boolean
+    }
+}
+</script>
+
+<style>
+
+</style>
--- pages/pokemon/index.vue
+++ pages/pokemon/index.vue
@@ -1,37 +1,46 @@
 <template>
-  <div class="flex">
-
+  <div class="flex p-5">
     <div class="flex items-left flex-col justify-left gap-5 w-6/12">
       <ul v-for="(item, index) in result" :key="item.name">
-        <li>
-            <button v-on:click="loadPokemon(index+1)">{{item.name}}</button>
+        <li class="flex gap-5 items-center justify-between">
+            <p class="name">{{item.name}}</p>
+            <button class="btn btn-primary" v-on:click="loadPokemon(index+1)">Ver</button>
         </li>
       </ul>
     </div>
 
-
-
-
-
-    <div class="w-6/12 max-h-screen overflow-y-auto">
-      <ul v-for="item in pokemon.moves" :key="item.name">
-        <li>
-            {{item.move.name}}
-        </li>
-      </ul>
+    <div class="w-6/12 max-h-screen overflow-y-auto" >
+        <pokemon :pokemon="pokemon" :show="show"/>
     </div>
   </div>
 
 </template>
 
+<style lang="scss" scoped>
+  li{
+    max-width: 250px;
+  }
+  .name{
+    color: white;
+    text-transform: capitalize;
+    font-size: 20px;
+  }
+</style>
 
 <script>  
+import pokemon from '../../components/pokemon.vue'
+
 export default {
+
   data(){
     return{
-      pokemon: []
+      pokemon: null,
+      show : false
     }
   },
+
+  components: { pokemon },
+ 
   async asyncData({ $axios }) {
     const result = (await $axios.$get('/pokemon/')).results
     return { result }
@@ -41,7 +50,11 @@ export default {
     async loadPokemon(id){
       let pokemon = await this.$axios.$get('https://pokeapi.co/api/v2/pokemon/'+id);
       this.pokemon = pokemon
+      this.pokemon.img = pokemon.sprites.front_default 
+
+      this.show = true
     }
   }
 }
 </script>
+
